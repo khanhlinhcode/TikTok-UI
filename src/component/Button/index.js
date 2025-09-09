9f9400e8e9d8e1a3727d27658b50358834de216a
Khanh Linh <linhto01012000cm@gmail.com>
2025-09-09 22:19:12 +0700

    commit len lan thu

--- src/component/Button/index.js
+++
@@ -1,7 +0,0 @@
-import myAlert from "~/myAlert";
-
-function Button() {
-  return <button onClick={myAlert}>Cick Me</button>;
-}
-
-export default Button;
--- src/myAlert.js
+++
@@ -1,5 +0,0 @@
-const myAlert = () => {
-  alert("Xin Chao Cac Ban");
-};
-
-export default myAlert;
